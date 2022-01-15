3e9fbef0183964de939630892835124fcf6188ee
jwang81 <junhuiwang2010@gmail.com>
2022-01-15 16:02:58 -0500

    twoSum.py

--- Easy/twoSum.py
+++ Easy/twoSum.py
@@ -1,4 +1,13 @@
 class Solution:
     def twoSum(self, nums, target):
-        print('helloWorld!')
-        print('firstPush')
+        hashmap = {}
+        for index,num in enumerate(nums):
+            another_num = target - num
+            if another_num in hashmap:
+                return [hashmap[another_num],index]
+            hashmap[num] = index
+#        print(hashmap)
+        return None
+
+sol = Solution()
+print(sol.twoSum([2,7,11,15],17))
