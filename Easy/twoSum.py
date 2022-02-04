845e1605e1adb6151dfec55e42a63c3168609a59
Jun wang81 <jwang81@lenovo.com>
2022-02-04 14:58:11 -0500

    twoSum.py

--- Easy/twoSum.py
+++ Easy/twoSum.py
@@ -1,3 +1,10 @@
+#Question 1:
+#(two sum):
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#link: https://leetcode-cn.com/problems/two-sum/
+
+##First way
 class Solution:
     def twoSum(self, nums, target):
         hashmap = {}
@@ -11,3 +18,23 @@ class Solution:
 
 sol = Solution()
 print(sol.twoSum([2,7,11,15],17))
+
+
+##second way
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :param nums: array of ints
+        :param target: int
+        :return: list[int]
+        """
+        indicesList = {}
+        for index, num in enumerate(nums):
+            if target - num in nums:
+                #print([index, num])
+                print(nums.index(target-num))
+                return [index, nums.index(target-num)]
+
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15],18))
+
