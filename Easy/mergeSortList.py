357e09b42df42430f54333e1c6898e491c2e3dd0
Jun wang81 <jwang81@lenovo.com>
2022-02-22 16:50:08 -0500

    listNode is a potential point

--- Easy/mergeSortList.py
+++ Easy/mergeSortList.py
@@ -9,6 +9,23 @@ class Solution:
         :param list2: list of int
         :return: list of int
         """
+        list3 = list1 + list2
+        list3.sort()
+        return list3
+
+mysol = Solution()
+print(mysol.mergeSortList([1, 2, 4], [1, 3, 4]))
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+class Solution:
+    def mergeSortList(self, list1, list2):
+        """
+        :param list1: list of int
+        :param list2: list of int
+        :return: list of int
+        """
         new1 = new2 = ListNode(0)
         while list1 and list2:
             if list1.val < list2.val:
@@ -24,5 +41,3 @@ class Solution:
 mysol = Solution()
 print(mysol.mergeSortList([1, 2, 4], [1, 3, 4]))
 
-# Input: list1 = [1,2,4], list2 = [1,3,4]
-# Output: [1,1,2,3,4,4]
